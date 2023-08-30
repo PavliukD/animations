20ed6ba7ca944ca55036998748b0b7cad01c2189
PavliukD <albatrozzz1@gmail.com>
2023-08-30 19:28:07 +0200

    animations

--- src/index.js
+++ src/index.js
@@ -4,27 +4,40 @@ import html from './index.html'
 Annimation()
 
 function Annimation() {
-    const wrap = document.querySelector('.animation')
+    const wrap = document.querySelectorAll('[data-animation=""]')
 
-    const elOne = wrap.querySelector('.animation-elemnt-one')
-    const elTwo = wrap.querySelector('.animation-elemnt-two')
+    wrap.forEach(el => {
+        const elOne = el.querySelector('.animation-elemnt-one')
+        const elTwo = el.querySelector('.animation-elemnt-two')
 
-    const steps = ['one', 'two', 'three', 'four', 'five', 'six']
 
-    let counter = 0
+        const steps = ['one', 'two', 'three', 'four', 'five', 'six']
+        const time = Number(el.dataset.time)
 
-    setInterval(() => {
+        let counter = 0
+
         elOne.classList.remove(steps[counter])
-        if (counter === steps.length - 1) {
-            counter = 0
+            elTwo.classList.remove(steps[counter])
+            counter++
             elOne.classList.add(steps[counter])
             elTwo.classList.add(steps[counter])
-            return
-        }
-        counter++
-        elOne.classList.add(steps[counter])
-        elTwo.classList.add(steps[counter])
-    }, 18000)
+
+        
+        setInterval(() => {
+            elOne.classList.remove(steps[counter])
+            elTwo.classList.remove(steps[counter])
+            if (counter === steps.length - 1) {
+                counter = 0
+                elOne.classList.add(steps[counter])
+                elTwo.classList.add(steps[counter])
+                return
+            }
+            counter++
+            elOne.classList.add(steps[counter])
+            elTwo.classList.add(steps[counter])
+        }, time)
+        
+    })
 }
 
 
